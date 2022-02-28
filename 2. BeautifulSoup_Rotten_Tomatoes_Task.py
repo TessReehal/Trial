448657ab64188097668fb78a896266f01f13950c
Merrin5 <100590776+Merrin5@users.noreply.github.com>
2022-02-28 14:49:29 +0000

    Update 2. BeautifulSoup_Rotten_Tomatoes_Task.py

--- 2. BeautifulSoup_Rotten_Tomatoes_Task.py
+++ 2. BeautifulSoup_Rotten_Tomatoes_Task.py
@@ -500,3 +500,4 @@ movie_review.to_csv('movie_review.csv', index = False, header = True)
 # Index is set to False so that the index (0,1,2...) of each movie is not saved to the file (the index is purely internal)
 # The header is set to True, so that the names of the columns are saved
 
+Hi Tess
